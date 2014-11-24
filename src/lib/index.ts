cc443d4923496f8cef5f571b58c97c06dcdf342c
Basarat Syed <bas@picnicsoftware.com>
2014-11-24 17:09:10 +1100

    work in progress. Starting naving the directory tree

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,26 @@
 /// <reference path="../typings/vendor.d.ts"/>
 /// <reference path="./interfaces.d.ts"/>
 
-import fs = require("fs");
+import fs = require('fs');
+import path = require('path');
+import yaml = require('js-yaml');
+
+var projectFileName = 'tsproj.yml'
 
 /** Given an src (source file or directory) goes up the directory tree to find all the project specifications that are impacted and then returns the parsed project specification. Use this to bootstrap the UI for what the user might want to do. */
 export function getProjectsSync(pathOrSrcFile: string): TypeScriptProjectFileDetails {
+
+    if (!fs.existsSync(pathOrSrcFile))
+        throw new Error('Invalid Path');
+
+    // Get the path directory
+    var dir = fs.lstatSync(pathOrSrcFile).isDirectory() ? pathOrSrcFile : path.dirname(pathOrSrcFile);
+
+    // Keep going up till we find the project file 
+    var projectFile = '';
+
+    
+
     return {
         projectFilePath: '',
         projects: []
